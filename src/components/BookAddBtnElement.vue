4f7c52dfc1691efef41b7e7f59886cf77cc2462c
vladyslavloktev <35151367+vladyslavloktev@users.noreply.github.com>
2020-10-27 11:51:49 +0100

    Add files via upload

---
+++ src/components/BookAddBtnElement.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="q-pa-md">
+    <q-page-container>
+      <q-page padding>
+
+        <q-page-sticky position="bottom-right" :offset="[18, 18]">
+          <q-fab
+            icon="add"
+            direction="up"
+            color="accent"
+          >
+            <q-fab-action @click="onDailyForm" type="button"  color="primary" icon="person_add"  />
+            <q-fab-action @click="onPainForm" color="primary" icon="las la-dizzy" />
+          </q-fab>
+        </q-page-sticky>
+      </q-page>
+    </q-page-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BookAddBtnElement',
+
+  methods: {
+    onDailyForm () {
+      this.$router.push('dailyForm')
+    },
+    onPainForm () {
+      this.$router.push('painForm')
+    }
+  }
+}
+</script>
---
+++ src/components/BookCardElement.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-item class="card q-mt-sm">
+    <q-item-section>
+      <q-item-label>Single line item</q-item-label>
+      <img class="card-img q-my-sm" src="https://cdn.quasar.dev/img/mountains.jpg">
+      <q-item-label  caption lines="1">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+    </q-item-section>
+
+    <q-item-section side top>
+      <q-item-label caption>5 min ago</q-item-label>
+        <q-card-actions vertical class="justify-around q-py-md" >
+        <q-btn flat round color="red" icon="create" @click="clickFav"/>
+        <q-btn flat round color="accent" icon="bookmark" @click="clickSave"/>
+        <q-btn flat round color="primary" icon="share" @click="clickShare"/>
+      </q-card-actions>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+
+export default {
+  name: 'BookCardElement',
+  methods: {
+    clickSave: function () {
+      return alert('save this post to favorites')
+    },
+    clickFav: function () {
+      return alert('i answer for edit function')
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+  .card-img {
+    max-width: 58vw;
+  }
+</style>
